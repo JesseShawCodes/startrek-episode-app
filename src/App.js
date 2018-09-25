fbc745a9f09f66c9f547557cecdf33856f82e3cf
Jesse Shaw <JesseShawCodes@gmail.com>
2018-09-25 15:31:22 -0400

    Updated Episode Component. Various Styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Episode from './Episode'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
+  }
+
+  componentDidMount() {
+    fetch('http://ec2-52-90-200-167.compute-1.amazonaws.com:8080')
+    .then(data => data.json())
+    .then(data => this.setState({dataReceived: true, data: data}))
+  }
+  
   render() {
+    var data = []
+    if (this.state.dataReceived === true) {
+      for (var i = 0; i < this.state.data.length; i++) {
+        data.push(<Episode key={i}
+          title={this.state.data[i].primaryTitle}
+          originalTitle={this.state.data[i].originalTitle}
+          episodeNumber={this.state.data[i].episodeNumber}
+          seasonNumber={this.state.data[i].seasonNumber}
+        />)
+      }
+      console.log("DATA RECEIVED!")
+    }
     return (
       <div className="App">
         <header className="App-header">
@@ -20,6 +44,9 @@ class App extends Component {
           <option value="3">6</option>
           <option value="4">7</option>
         </select>
+        <div className="data-section">
+          {data}
+        </div>
       </div>
     );
   }
---
+++ src/Episode.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Episode.css';
+
+class Episode extends Component {
+  render() {
+    return (
+      <div className="episode">
+        <h3>{this.props.title}</h3>
+        <span className="title">{this.props.originalTitle}</span>
+        <span className="episode-number">Episode: {this.props.episodeNumber}</span>
+        <span className="season-number">Season: {this.props.seasonNumber}</span>
+        <div className="average-rating">Rating:</div>
+        <div className="number-votes">Number Votes:</div>
+      </div>
+    );
+  }
+}
+
+export default Episode;
