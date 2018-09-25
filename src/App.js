6dd99285eb7cb2a0605f766d802e00ff6ba0c1aa
Jesse Shaw <JesseShawCodes@gmail.com>
2018-09-25 14:27:54 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <h2>Select Season</h2>
+        <select>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="2">5</option>
+          <option value="3">6</option>
+          <option value="4">7</option>
+        </select>
+      </div>
+    );
+  }
+}
+
+export default App;
