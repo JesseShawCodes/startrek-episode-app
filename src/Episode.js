5b5e2754bbd96eabe9e02597c58fbdb33225872b
Jesse Shaw <JesseShawCodes@gmail.com>
2018-09-25 20:58:58 -0400

    Updated Badge Styling

--- src/Episode.js
+++ src/Episode.js
@@ -5,12 +5,19 @@ class Episode extends Component {
   render() {
     return (
       <div className="episode">
-        <h3>{this.props.title}</h3>
-        <span className="title">{this.props.originalTitle}</span>
+        <h3>{this.props.originalTitle}</h3>
         <span className="episode-number">Episode: {this.props.episodeNumber}</span>
         <span className="season-number">Season: {this.props.seasonNumber}</span>
-        <div className="average-rating">Rating: {this.props.rating}</div>
-        <div className="number-votes">Number Votes: {this.props.votes}</div>
+        <section className="badges">
+          <div className="average-rating">
+            <span className="badge-label">Rating:</span>
+            <span className="badge-rating">{this.props.rating}</span>
+          </div>
+          <div className="number-votes">
+            <span className="badge-label">Votes:</span> 
+            <span className="badge-votes">{this.props.votes}</span>
+          </div>
+        </section>
       </div>
     );
   }
